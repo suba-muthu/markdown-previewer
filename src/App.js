4c742f338ed42b603f51093e1f1c7654d7f345ec
suba <subalakshmimuthu02@gmail.com>
2023-07-18 18:01:05 +0530

    complete markdown previewer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import { marked } from "marked";
+import './index.css';
 
 function App() {
+
+  const [ content, setContent ] = useState(
+  `
+  # React Markdown Previewer!
+
+  ## This is a sub-heading...
+      
+  Or... wait for it... **_both!_**
+    
+  And feel free to go crazy ~~crossing stuff out~~.
+        
+  There's also [link](https://www.freecodecamp.org)
+  `   
+  );
+
+  function handleChange(e) {
+    setContent(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div id="edit-container">
+        <h2>Editor</h2>
+        <textarea id="editor" value={content} onChange={handleChange}></textarea>
+      </div>
+      <div id="preview-container">
+        <h2 id="heading">Previewer</h2>
+        <div 
+          id="preview"
+          dangerouslySetInnerHTML={{
+            __html: marked(content),
+          }}
+        ></div>
+      </div>
     </div>
   );
 }
